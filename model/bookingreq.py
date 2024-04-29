52959204e02d4700c9a36148fc493623afea38b9
rock4rd <rdagohoy_220000000565@uic.edu.ph>
2024-04-29 15:43:17 +0800

    asd

--- model/bookingreq.py
+++ model/bookingreq.py
@@ -9,29 +9,30 @@ BookingRouter = APIRouter(tags=["Booking Requests"])
 
 # CRUD operations
 
-@BookingRouter.get("/bookingrequests/", response_model=list)
-async def read_bookingrequests(
+@BookingRouter.get("/bookings/", response_model=list)
+async def read_bookings(
     db=Depends(get_db)
 ):
-    query = "SELECT requestid, userid ,Firstname, Lastname, purpose, daterequested,status FROM bookingrequests"
-    db[0].execute(query)
-    bookingrequests = [{"requestid": bookingrequests[0], "userid": bookingrequests[1], "Firstname": bookingrequests[2],"Lastname": bookingrequests[3],"purpose": bookingrequests[4],"daterequested": bookingrequests[5],"status": bookingrequests[6]} for bookingrequests in db[0].fetchall()]
-    return bookingrequests
+    query = "SELECT b.DateIn, b.TimeIn, b.TimeOut, g.FirstName, g.LastName, b.Purpose FROM bookings b INNER JOIN guests g ON b.GuestID = g.GuestID"
 
-@BookingRouter.get("/bookingrequests/{requestid}", response_model=dict)
-async def read_bookingrequests(
+    db[0].execute(query)
+    bookings = [{"DateIN": booking[0], "TimeIn": booking[1], "TimeOut": booking[2],"FirstName": booking[3],"LastName": booking[4],"Purpose": booking[5]} for booking in db[0].fetchall()]
+    return bookings
+
+@BookingRouter.get("/bookings/{requestid}", response_model=dict)
+async def read_bookings(
     requestid: int, 
     db=Depends(get_db)
 ):
-    query = "SELECT  requestid, userid ,Firstname, Lastname, purpose, daterequested,status FROM bookingrequests WHERE requestid = %s"
+    query = "SELECT  requestid, userid ,Firstname, Lastname, purpose, daterequested,status FROM bookings WHERE requestid = %s"
     db[0].execute(query, (requestid,))
-    bookingrequests = db[0].fetchone()
-    if  bookingrequests:
-        return {"requestid": bookingrequests[0], "userid": bookingrequests[1], "Firstname": bookingrequests[2],"Lastname": bookingrequests[3],"purpose": bookingrequests[4],"daterequested": bookingrequests[5],"status": bookingrequests[6]}
-    raise HTTPException(status_code=404, detail="bookingrequests not found")
+    bookings = db[0].fetchone()
+    if  bookings:
+        return {"requestid": bookings[0], "userid": bookings[1], "Firstname": bookings[2],"Lastname": bookings[3],"purpose": bookings[4],"daterequested": bookings[5],"status": bookings[6]}
+    raise HTTPException(status_code=404, detail="bookings not found")
 
-@BookingRouter.post("/bookingrequests/", response_model=dict)
-async def create_bookingrequests(
+@BookingRouter.post("/bookings/", response_model=dict)
+async def create_bookings(
     Firstname: str = Form(...),
     Lastname: str = Form(...),
     purpose: str = Form(...),
@@ -41,8 +42,8 @@ async def create_bookingrequests(
 ):
     # Hash the password using bcrypt
    
-    # Insert the bookingrequests record into the database
-    query = "INSERT INTO bookingrequests (Firstname, Lastname, purpose, daterequested,status) VALUES (%s, %s, %s, %s, %s)"
+    # Insert the bookings record into the database
+    query = "START TRANSACTION; INSERT INTO guests (firstname, lastname) VALUES (%s, %s) INSERT INTO bookings (guest_id, purpose, daterequested, status) VALUES (LAST_INSERT_ID(), %s, %s, %s) COMMIT"
     db[0].execute(query, (Firstname, Lastname, purpose, daterequested,status))
 
     # Retrieve the last inserted ID using LAST_INSERT_ID()
@@ -54,8 +55,8 @@ async def create_bookingrequests(
 
 
 
-@BookingRouter.put("/bookingrequests/{requestid}", response_model=dict)
-async def update_bookingrequests(
+@BookingRouter.put("/bookings/{requestid}", response_model=dict)
+async def update_bookings(
     Firstname: str = Form(...),
     Lastname: str = Form(...),
     purpose: str = Form(...),
@@ -68,37 +69,37 @@ async def update_bookingrequests(
 
 
     # Update user information in the database 
-    query = "UPDATE bookingrequests SET Firstname= %s, Lastname = %s, purpose = %s, daterequested = %s, status = %s WHERE requestid = %s"
+    query = "UPDATE bookings SET Firstname= %s, Lastname = %s, purpose = %s, daterequested = %s, status = %s WHERE requestid = %s"
     db[0].execute(query, ( Firstname,Lastname, purpose,daterequested,status ))
 
     # Check if the update was successful
     if db[0].rowcount > 0:
         db[1].commit()
-        return {"message": "bookingrequests updated successfully"}
+        return {"message": "bookings updated successfully"}
     
     # If no rows were affected, user not found
-    raise HTTPException(status_code=404, detail="bookingrequests not found")
+    raise HTTPException(status_code=404, detail="bookings not found")
 
-@BookingRouter.delete("/bookingrequests/{requestid}", response_model=dict)
-async def delete_bookingrequests(
+@BookingRouter.delete("/bookings/{requestid}", response_model=dict)
+async def delete_bookings(
     requestid: int,
     db=Depends(get_db)
 ):
     try:
         # Check if the user exists
-        query_check_user = "SELECT requestid FROM bookingrequests WHERE requestid = %s"
+        query_check_user = "SELECT requestid FROM bookings WHERE requestid = %s"
         db[0].execute(query_check_user, (requestid,))
         existing_user = db[0].fetchone()
 
         if not existing_user:
-            raise HTTPException(status_code=404, detail="bookingrequests not found")
+            raise HTTPException(status_code=404, detail="bookings not found")
 
         # Delete the user
-        query_delete_user = "DELETE FROM bookingrequests WHERE requestid = %s"
+        query_delete_user = "DELETE FROM bookings WHERE requestid = %s"
         db[0].execute(query_delete_user, (requestid,))
         db[1].commit()
 
-        return {"message": "bookingrequests deleted successfully"}
+        return {"message": "bookings deleted successfully"}
     except Exception as e:
         # Handle other exceptions if necessary
         raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
